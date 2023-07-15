bb3fd0fb5dc8364942ec6ce35c1733fa1d2d4ac9
bichnguyen292 <nguyenthibich290292@gmail.com>
2023-07-15 13:47:35 +0700

    update

--- webassets/js/javascript.js
+++ webassets/js/javascript.js
@@ -1,36 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function () { });
 
-})
+var heightPagination = document.querySelector('.swiper-pagination').offsetHeight;
 
 var swiper = new Swiper(".swiper-banner", {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
     },
 });
-var slideImages = document.querySelectorAll(".mySwiper .swiper-slide .swiper-content");
+var slideImages = document.querySelectorAll(
+    ".swiper-banner .swiper-slide .swiper-content"
+);
 var maxHeight = 0;
 slideImages.forEach(function (image) {
     if (image.offsetHeight > maxHeight) {
         maxHeight = image.offsetHeight;
     }
 });
-var swiperContainer = document.querySelector(".mySwiper");
+var swiperContainer = document.querySelector(".swiper-banner");
 swiperContainer.style.height = maxHeight + "px";
 
-
 var swiper = new Swiper(".swiper-2row", {
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 18,
     grid: {
-        rows: 2,
+        rows: 1,
     },
-    spaceBetween: 30,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 18,
+            grid: {
+                rows: 1,
+            },
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 18,
+            grid: {
+                rows: 1,
+            },
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 18,
+            grid: {
+                rows: 2,
+            },
+        },
+    },
 });
-var slideImages1 = document.querySelectorAll(".swiper-2row .swiper-slide .swiper-content");
+
+var slideImages1 = document.querySelectorAll(
+    ".swiper-2row .swiper-slide .swiper-content"
+);
 var maxHeight1 = 0;
 slideImages1.forEach(function (image) {
     if (image.offsetHeight > maxHeight1) {
@@ -38,16 +66,33 @@ slideImages1.forEach(function (image) {
     }
 });
 var swiperContainer1 = document.querySelector(".swiper-2row");
-swiperContainer1.style.height = (maxHeight1*2 + 50) + "px";
-
+swiperContainer1.style.height = maxHeight1 * 2 + heightPagination + 80 + "px";
 
 var swiper = new Swiper(".swiper-normal", {
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
         el: ".swiper-pagination",
-        dynamicBullets: true,
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 22,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 22,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 22,
+        },
     },
 });
-var slideImages2 = document.querySelectorAll(".swiper-normal .swiper-slide .swiper-content");
+var slideImages2 = document.querySelectorAll(
+    ".swiper-normal .swiper-slide .swiper-content"
+);
 var maxHeight2 = 0;
 slideImages2.forEach(function (image) {
     if (image.offsetHeight > maxHeight2) {
@@ -55,4 +100,4 @@ slideImages2.forEach(function (image) {
     }
 });
 var swiperContainer2 = document.querySelector(".swiper-normal");
-swiperContainer2.style.height = maxHeight2 + "px";
+swiperContainer2.style.height = maxHeight2 + heightPagination + "px";
